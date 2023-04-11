00e7192ffda0b3431292a6f901ca571e53bc2705
eziraa <ezratgab@gmail.com>
2023-04-11 04:24:07 +0300

    commited

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,8 +8,8 @@ def is_same_class(obj, a_class):
         obj (any) which is an object that can be tested
         a_class(type) is the given class indication
     Returns:
-        Ture if the object is instance of the  given class  or false if not
-         """
+        Ture if the object is instance of the  given class  or false if not.
+    """
     if type(obj) == a_class:
         return True
     else:
