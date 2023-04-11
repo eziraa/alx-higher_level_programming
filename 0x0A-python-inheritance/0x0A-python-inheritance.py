0bb1c8750778039015779a9ec34452ce7de4052b
eziraa <ezratgab@gmail.com>
2023-04-11 04:03:03 +0300

    commited

---
+++ 0x0A-python-inheritance/0x0A-python-inheritance.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton
+"""Define  a class with its funciton which sort a lis in asecnding order."""
+
+
+class MayList(list):
+    pass
+    # defining a function which sort a list
+
+    def print_sorted(self):
+        print(self.sort)
