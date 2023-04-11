0b908a48870d06d65100015fb9a550776f3fe818
eziraa <ezratgab@gmail.com>
2023-04-11 04:26:18 +0300

    commited

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Defing a function thst check weather or object is instance of the the given class"""
+"""Defing a function thst check weather or object is instance of the the given class."""
 
 
 def is_same_class(obj, a_class):
-    """Chceck is the object is the instance of the given class
+    """Chceck is the object is the instance of the given class.
+
     Args : 
         obj (any) which is an object that can be tested
         a_class(type) is the given class indication
