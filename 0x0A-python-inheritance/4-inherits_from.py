184c9780d7c7c57409afbb6649a1e633243e8fae
eziraa <ezratgab@gmail.com>
2023-04-11 04:57:25 +0300

    commited

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,11 +6,11 @@ def inherits_from(obj, a_class):
     """This function defin  whether one class is the sub class of the other object.
 
     Args :
-        obj(any) the object we want to check.
-        a_class(type) the super class of the object we will check.
+        obj(any): the object we want to check.
+        a_class(type): the super class of the object we will check.
     Returns :
         if  the class is the super class of the object class.
-        else return false
+        else return false.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
