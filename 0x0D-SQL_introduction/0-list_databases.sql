b19cfee3effd14ee4c858da45af5afd97a731123
eziraa <ezratgab@gmail.com>
2023-05-17 01:13:05 +0300

    The project is done

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,22 +1,2 @@
---this file to write the script that list all users in databse
-import mysql.connector
-
-# Connect to the MySQL server
-cnx = mysql.connector.connect(user='your_username', password='your_password', host='your_host', database='your_database')
-
-# Create a cursor to execute SQL queries
-cursor = cnx.cursor()
-
-# Execute the query to get all databases
-cursor.execute("SHOW DATABASES")
-
-# Fetch all the results
-databases = cursor.fetchall()
-
-# Print the list of databases
-for db in databases:
-    print(db[0])
-
-# Close the cursor and connection
-cursor.close()
-cnx.close()
+-- Lists all databases of my MySQL server
+SHOW DATABASES;
